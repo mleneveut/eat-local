d71120136f7c19a4b11f430e835d88ea80ef7d15
afillatre <afillatre@ippon.fr>
2014-01-20 18:39:18 +0100

    Update model

--- models/producteur.js
+++ models/producteur.js
@@ -2,7 +2,7 @@
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
-    Schema = mongoose.Schema,
+    Schema = mongoose.Schema,   
     ObjectId = Schema.ObjectId;
 
 var ProducteurSchema = mongoose.Schema({
