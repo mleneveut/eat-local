db6c89f14b40af05793f1584b2262c0225eccb44
afillatre <afillatre@ippon.fr>
2014-01-20 15:55:59 +0100

    Add nodeJS

---
+++ config/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    development: {
+        root: require('path').normalize(__dirname + '/..'),
+        host: "127.0.0.1",
+        port: "80",
+        db_prefix: "mongodb",
+        db_host: "10.55.0.79",
+        db_port: "27017",
+        db_database: "eat-local"
+    }
+}
---
+++ models/producteur.js
@@ -0,0 +1,21 @@
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose')
+    , Schema = mongoose.Schema
+    , ObjectId = Schema.ObjectId;
+
+var CoordonneesSchema = mongoose.Schema({
+    latitude: Number,
+    longitude: Number
+});
+
+var ProducteurSchema = mongoose.Schema({
+    id: ObjectId,
+    nom: String,
+    description: String,
+    raison_sociale: String,
+    coordonnees: [CoordonneesSchema]
+});
+
+module.exports = mongoose.model("producteurs", ProducteurSchema);
